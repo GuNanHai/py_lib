9277a9bb5dbdfdaed600e0728b24e7b2e82b8184
guhai <bysideen@gmail.com>
2019-04-01 19:27:09 +0800

    upload async_requests_Demo.py

---
+++ async_requests_Demo.py
@@ -0,0 +1,44 @@
+import requests
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+
+urls = [
+    'https://api.ipify.org/?format=json',
+    'http://pv.sohu.com/cityjson',
+    'https://jsonip.com/'
+]
+
+def fetch(session,url):
+    try:
+        with session.get(url,timeout=1) as response:
+            pageContent = response.content
+            if response.status_code != 200:
+                print("连接失败：{0}".format(url))
+            return pageContent
+    except Exception as e:
+        print(e)
+
+
+async def get_data_asynchronous(urls):
+    pageContent = []
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        with requests.Session() as session:
+            loop = asyncio.get_event_loop()
+            tasks = [
+                loop.run_in_executor(
+                    executor,
+                    fetch,
+                    *(session,url)
+                )
+                for url in urls
+            ]
+            for response in await asyncio.gather(*tasks):
+                pageContent.append(response)
+            return pageContent
+
+
+loop = asyncio.get_event_loop()
+future = asyncio.ensure_future(get_data_asynchronous(urls))
+test = loop.run_until_complete(future)
+
+
