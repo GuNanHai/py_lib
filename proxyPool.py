ce199e1229910496286a2e3999621cc51f83aa13
guhai <bysideen@gmail.com>
2019-04-03 15:15:51 +0800

    update

--- proxyPool.py
+++ proxyPool.py
@@ -23,7 +23,10 @@ os.environ['no_proxy']='*'
 def getIpList(ipcount=300):
     ipCount = ipcount
     #此ip接口不能使用代理连接
-    api = 'http://www.66ip.cn/nmtq.php?getnum='+str(ipcount)+'&isp=0&anonymoustype=0&start=&ports=&export=&ipaddress=&area=0&proxytype=1&api=66ip'
+    # 下面这个被注释掉的api用于https代理的获取
+    # api = 'http://www.66ip.cn/nmtq.php?getnum='+str(ipcount)+'&isp=0&anonymoustype=0&start=&ports=&export=&ipaddress=&area=0&proxytype=1&api=66ip'
+
+    api = 'http://www.66ip.cn/mo.php?sxb=&tqsl='+str(ipcount)+'&port=&export=&ktip=&sxa=&submit=%CC%E1++%C8%A1&textarea=http%3A%2F%2Fwww.66ip.cn%2F%3Fsxb%3D%26tqsl%3D10%26ports%255B%255D2%3D%26ktip%3D%26sxa%3D%26radio%3Dradio%26submit%3D%25CC%25E1%2B%2B%25C8%25A1'
     response = requests.get(api)
     htmlContent = etree.HTML(response.content)
     rowStringList = htmlContent.xpath('//body/p/text()') 
@@ -50,7 +53,8 @@ def fetch(session,url,headers,proxy,timeout=3):
 async def get_data_asynchronous(ipList,protocol,timeout=3):
     proxies = []
     responses = []
-    url = 'https://api.ipify.org/?format=json'
+    # url = 'https://api.ipify.org/?format=json'
+    url = 'http://pv.sohu.com/cityjson'
     userAgents = [ "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
                 "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
                 "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0",
@@ -78,7 +82,7 @@ async def get_data_asynchronous(ipList,protocol,timeout=3):
             return responses
 
 # 输入： 【IP:Port】
-def refineProxies(ipList,protocol='https',timeout=3):
+def refineProxies(ipList,protocol='http',timeout=3):
     loop = asyncio.get_event_loop()
     future = asyncio.ensure_future(get_data_asynchronous(ipList,protocol,timeout))
     responses = loop.run_until_complete(future)
@@ -91,15 +95,7 @@ def refineProxies(ipList,protocol='https',timeout=3):
     with open('proxyPool.json','w') as f:
         json.dump(proxyPool,f)
 
-# 测试用： 将{'https':'https://192.168.0.1:8080'}的格式转化为【IP:Port】----用于检查本地代理IP池的可用性。
-def convert_proxy_format(file):
-    with open(file,'r') as f:
-        proxyPool = json.load(f)
-    newProxyPool = []
-    for each in proxyPool:
-        if each['https']:
-            newProxyPool.append(each['https'].replace('https://',''))
-    return newProxyPool
+
 # 功能，合并多个代理池，
 # 输入：json文件名
 def merge_proxyPools(*proxyPools):
@@ -110,12 +106,31 @@ def merge_proxyPools(*proxyPools):
             finalProxyPool.extend(temp)
     with open('proxyPool.json','w') as f:
         json.dump(finalProxyPool,f)
-    
+
+# 以下专用于https代理的获取
+# def main():
+#     refineProxies(getIpList(),'https')
+
 def main():
-    refineProxies(getIpList(),'https')
+    refineProxies(getIpList(10000),'http')
 
 
 
+
+# =============================================测试本地代理可用性
+# 测试用： 将{'https':'https://192.168.0.1:8080'}的格式转化为【IP:Port】----用于检查本地代理IP池的可用性。
+def convert_proxy_format(file='proxyPool.json'):
+    with open(file,'r') as f:
+        proxyPool = json.load(f)
+    newProxyPool = []
+    for each in proxyPool:
+        if each['http']:
+            newProxyPool.append(each['http'].replace('http://',''))
+    return newProxyPool
+
+# 开启测试：测试本地代理池可用性。
+def startTest():
+    refineProxies(convert_proxy_format())
 
 
 
