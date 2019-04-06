171d2989586016f15ad64812646067052b656960
guhai <bysideen@gmail.com>
2019-04-06 19:10:36 +0800

    update

---
+++ testConnectLocalhost.py
@@ -0,0 +1,25 @@
+
+import asyncio 
+from aiohttp import ClientSession 
+ 
+async def fetch(url):      
+   async with ClientSession() as session:           
+      async with session.get(url) as response:             
+          return await response.read()             
+          
+async def run(loop,  r):     
+   url = "http://localhost:8080/{}"         
+   tasks = []         
+   for i in range(r):             
+       task = asyncio.ensure_future(fetch(url.format(i)))                 
+       tasks.append(task)         
+       responses = await asyncio.gather(*tasks)         
+       # you now have all response bodies in this variable         
+       pass  
+       
+def print_responses(result):       
+    pass    
+    
+loop = asyncio.get_event_loop() 
+future = asyncio.ensure_future(run(loop, 1000)) 
+loop.run_until_complete(future)
