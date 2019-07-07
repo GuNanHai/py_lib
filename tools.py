9e507e0bf7b42443007decd006bafd8058b32626
GuNanHai <42151384+GuNanHai@users.noreply.github.com>
2019-07-07 14:02:39 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -12,27 +12,29 @@ cwd = os.getcwd()
 
 
 
-# 获取当前路径下的所有文件夹的路径，按照数字小到大排列
+# 获取当前路径下的所有文件夹的路径，按照数字小到大排列(假定序号在dir名开头，且序号后文件名中不再包含数字）
 def getDirList(path=cwd):
     dirList = []
     allDir = os.listdir(path)
-    #-------------剔除非数字名称的文件夹
-    temp = []
+    def getNum(val):
+        baseName = os.path.basename(val)
+        baseName = baseName.split('.')[0]
+        result = ''
+        for each in baseName:
+            if each.isdigit():
+                result = result + each
+        if result=='':
+            result=9999999
+        return result
+    allDir.sort(key=lambda f: int(getNum(f)))
     for each in allDir:
-        if os.path.basename(each).isdigit():
-            temp.append(each)
-    allDir = temp
-    #-------------------------------
-    allDir.sort(key=lambda f: int(os.path.basename(f)))
-    for each in allDir:
-        if each == '__pycache__':
-            continue
         each = path + FPSLASH + each
         if os.path.isdir(each):
             dirList.append(each)
     return dirList
 
-# 获取当前路径下的所有文件的路径，按照数字小到大排列
+
+# 获取当前路径下的所有文件的路径，按照数字小到大排列(假定序号在fuke名开头，且序号后file名中不再包含数字）
 def getFileList(path=cwd):
     fileList = []
     allDir = os.listdir(path)
