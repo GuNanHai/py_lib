5e4ea2c665554bbc38ca6e6f52d4f4a6a948fbaa
GuNanHai <42151384+GuNanHai@users.noreply.github.com>
2019-07-07 13:01:58 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,4 +1,13 @@
 import os
+import sys
+
+# 依照系统判断文件路径分离符号
+osPlatform = sys.platform
+if osPlatform == 'win32':
+    FPSLASH = '\\'
+if osPlatform == 'linux1' or osPlatform == 'linux2':
+    FPSLASH = '/'
+    
 cwd = os.getcwd()
 
 
@@ -18,25 +27,29 @@ def getDirList(path=cwd):
     for each in allDir:
         if each == '__pycache__':
             continue
-        each = path + '/' + each
+        each = path + FPSLASH + each
         if os.path.isdir(each):
             dirList.append(each)
     return dirList
 
-
+# 获取当前路径下的所有文件的路径，按照数字小到大排列
 def getFileList(path=cwd):
     fileList = []
     allDir = os.listdir(path)
     def getNum(val):
         baseName = os.path.basename(val)
+        baseName = baseName.split('.')[0]
         result = ''
         for each in baseName:
             if each.isdigit():
                 result = result + each
+        if result=='':
+            result=9999999
         return result
     allDir.sort(key=lambda f: int(getNum(f)))
     for each in allDir:
-        each = path + '/' + each
+        each = path + FPSLASH + each
         if os.path.isfile(each):
             fileList.append(each)
     return fileList
+
